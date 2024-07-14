e7a31f3fa1c35e207178d768b209e70a74aae427
MedNewton <med.newton.2013@gmail.com>
2024-07-14 14:28:53 -0700

    .

--- dist/index2.199929a3.js
+++ dist/index2.199929a3.js
@@ -39755,7 +39755,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65374" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57667" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
